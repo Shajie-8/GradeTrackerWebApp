ed3ddfaa74e5482b51e8b904be83f2cf6767f9f9
Shajie-8 <shajie.macabalang@gmail.com>
2026-07-22 20:38:41 +0800

    Renamed files

--- src/main/java/ph/edu/dlsu/lbycpob/gradetrackerwebapp/GradeTrackerWebApp.java
+++ src/main/java/ph/edu/dlsu/lbycpob/gradetrackerwebapp/GradeTrackerWebApp.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GradeTrackerWebApp {
     static void main(String[] args) {
-        SpringApplication.run(GradeTrackerWebAppApplication.class, args);
+        SpringApplication.run(GradeTrackerWebApp.class, args);
     }
 }
